01a75420802e8eb205ec578617e2aa651dd105fe
wangyanchao <wangyanchao@me.com>
2019-04-19 10:30:23 +0800

    add bear file

---
+++ BearDemo/BearDemo/Bear.swift
@@ -0,0 +1,13 @@
+//
+//  Bear.swift
+//  BearDemo
+//
+//  Created by wangyanchao on 2019/4/19.
+//  Copyright © 2019 wangyanchao. All rights reserved.
+//
+
+import UIKit
+
+class Bear: NSObject {
+
+}
