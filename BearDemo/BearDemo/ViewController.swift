6c7631aa4cc5963480dd895ba8c1fe77f130bb4d
wangyanchao <wangyanchao@me.com>
2019-04-19 10:23:57 +0800

    bear demo init

---
+++ BearDemo/BearDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BearDemo
+//
+//  Created by wangyanchao on 2019/4/19.
+//  Copyright © 2019 wangyanchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.green
+    }
+
+
+}
+
