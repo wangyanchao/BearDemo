dd08edbb4a56553de019fb2597d959e853a6fa4d
wangyanchao <wangyanchao@me.com>
2019-05-07 09:47:15 +0800

    hellobear modify

--- BearDemo/BearDemo/ViewController.swift
+++ BearDemo/BearDemo/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.green
         self.view.tag = 10
-        print("hello")
+        print("hello bear")
         
     }
 
